16f3075038edad0a4dd51a2ac3b91610b1f61c28
sangyeongshin <tlstkddud12@naver.com>
2025-03-07 21:04:01 +0900

    style commit

--- JS/common.js
+++ JS/common.js
@@ -1,17 +1,53 @@
-let newsList = [];
-const menus = document.querySelectorAll(".menus button");
-menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)));
-const sideNav = document.querySelectorAll(".side-nav button");
-sideNav.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)));
 
 document.addEventListener("DOMContentLoaded", function () {
-    const btnBar = document.querySelector(".btn-bar");
-    const gnb = document.querySelector(".gnb");
-    let isMenuOpen = false;
-
-    btnBar.addEventListener("click", function () {
-        isMenuOpen = !isMenuOpen;
-        gnb.classList.toggle("active", isMenuOpen);
+    const menuItems = document.querySelectorAll(".side-menu-list a");
+    menuItems.forEach(item => {
+        item.addEventListener("click", (event) => {
+            event.preventDefault(); 
+            getNewsByCategory(event);
+        });
     });
 
 });
+
+const openNav = () => {
+    document.getElementById("mySidenav").style.width = "100%";
+};
+
+const closeNav = () => {
+    document.getElementById("mySidenav").style.width = "0";
+};
+
+// 검색
+
+const searchButton = document.getElementById("btnSearch"); 
+const searchWrap = document.getElementById("search-list"); 
+let isSearchOpen = false; 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    searchButton.addEventListener("click", function () {
+        isSearchOpen = !isSearchOpen; 
+        if (isSearchOpen) {
+            searchWrap.style.display = "flex"; 
+            searchWrap.style.opacity = "1"; 
+        } else {
+            searchWrap.style.opacity = "0"; 
+            setTimeout(() => {
+                searchWrap.style.display = "none"; 
+            }, 500); 
+        }
+    });
+
+    searchWrap.addEventListener("click", function (event) {
+        if (event.target === searchWrap) {
+            isSearchOpen = false;
+            searchWrap.style.opacity = "0";
+            setTimeout(() => {
+                searchWrap.style.display = "none";
+            }, 500);
+        }
+    });
+});
+
+
